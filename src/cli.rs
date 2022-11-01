5da33bf15a6a0498e181c9435cf764b73fd8019a
Johannes Horner <jhn5.horner@gmail.com>
2022-11-01 16:58:10 +0100

    add interactive functionionality

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use crate::champions::CHAMPIONS;
 use crate::generator::get_random_champions_from_pool;
 use clap::Parser;
@@ -5,7 +7,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    amount: usize,
+    amount: Option<usize>,
 
     #[arg(short, long, value_name = "CHAMPION1,CHAMPION2")]
     champions: Option<String>,
@@ -20,11 +22,21 @@ pub fn run() {
         CHAMPIONS.to_vec()
     };
 
-    if args.amount > champion_pool.len() {
-        println!("Amount of random champions {{{}}} can't be greater than the size of the specified champion pool {{{}}}!", args.amount, champion_pool.len());
+    let amount = if let Some(amount) = args.amount {
+        amount
+    }
+    else {
+        print!("Number of champions to generate: "); 
+        std::io::stdout().flush().unwrap();
+        let line = std::io::stdin().lines().next().unwrap().unwrap(); 
+        line.parse::<usize>().unwrap()
+    };
+
+    if amount > champion_pool.len() {
+        println!("Amount of random champions {{{}}} can't be greater than the size of the specified champion pool {{{}}}!", amount, champion_pool.len());
         return;
     }
 
-    let random_champions = get_random_champions_from_pool(args.amount, &champion_pool);
+    let random_champions = get_random_champions_from_pool(amount, &champion_pool);
     println!("{}", random_champions.join("\n"));
 }
